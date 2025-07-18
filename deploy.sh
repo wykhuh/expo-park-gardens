21198cba48fd3e48d41ee3f5c91e473a92f76797
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2025-07-18 05:30:53 -0700

    add deploy bash script

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# https://stackoverflow.com/a/37890564
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$BRANCH" != "main" ]];
+then
+  echo 'Aborting script; not on main';
+  exit 1;
+fi
+
+changes="$(git diff-index HEAD --)"
+if [[ "$changes" != "" ]];
+then
+  echo 'Aborting script; unstaged changes';
+  exit 1;
+fi
+echo 'Do stuff!';
+
+git branch -D gh-pages
+git checkout -b gh-pages
+
+
+quarto render
+mv ./_site/scripts docs
+rm -rf ./_site
+rm -rf ./data
+rm -rf ./scripts
+rm gardens.Rproj
+rm renv.lock
+rm _quarto.yml
+git add .
+git commit -m 'update site'
+git push origin gh-pages -f
+git checkout main
